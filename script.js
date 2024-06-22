2e1e52e92de26107e5aca74987e07157ef88335d
Victor <victormachado8@hotmail.com>
2024-06-22 20:59:40 -0300

    config do rum

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const clickButton = document.getElementById('clickButton');
 const clickCount = document.getElementById('clickCount');
-const apiResponse = document.getElementById('apiResponse'); // Adicionado para mostrar a resposta da API
+const apiResponse = document.getElementById('apiResponse');
 
 let count = 0;
 
@@ -8,8 +8,6 @@ clickButton.addEventListener('click', () => {
   count++;
   clickCount.textContent = `Cliques: ${count}`;
   
-
-  // para testar utilizar o seguinte link:  https://cors-anywhere.herokuapp.com/corsdemo  -  O erro que você está enfrentando é devido ao uso do CORS Anywhere, que tem uma limitação de uso e requer que você primeiro desbloqueie o proxy acessando uma página de demonstração
   // URL do proxy CORS Anywhere
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
   const targetUrl = 'http://3.238.219.121:3000/msg';
@@ -23,5 +21,4 @@ clickButton.addEventListener('click', () => {
     .catch(error => {
       apiResponse.textContent = `Erro ao chamar a API: ${error}`;
     });
-    console.log(apiResponse.textContent);
 });
