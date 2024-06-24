e86a2b0dd068374bf582ec200bc9f52bb39fb699
Andrei B Rodrigues <andreibr@gmail.com>
2024-06-24 10:45:41 -0300

    tentativa uso cors como options na chamada fetch

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const clickButton = document.getElementById('clickButton');
 const clickCount = document.getElementById('clickCount');
 const apiResponse = document.getElementById('apiResponse');
 
+const options = {
+  method: 'GET',
+  url: 'http://3.238.219.121:3000/msg',
+  Headers: {
+    'Content-Type': 'application/json',
+  },
+  mode: 'no-cors',
+  cache: 'no-cache',
+  credentials: 'same-origin',
+  redirect: 'follow',
+  referrerPolicy: 'no-referrer',
+};
+
 let count = 0;
 
 clickButton.addEventListener('click', () => {
@@ -11,11 +24,12 @@ clickButton.addEventListener('click', () => {
   
   // teste
   // URL do proxy CORS Anywhere
-  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+  // const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
   const targetUrl = 'http://3.238.219.121:3000/msg';
   
   // Chamada da API via proxy
-  fetch(proxyUrl + targetUrl)
+  // fetch(targetUrl)
+  fetch(targetUrl, options)
     .then(response => response.json())
     .then(data => {
       apiResponse.textContent = `Resposta da API: ${data.message}`;
