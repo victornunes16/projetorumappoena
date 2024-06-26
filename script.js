f3ceabc0d281a68f0e5091aa832056c31ad7b510
Andrei B Rodrigues <andreibr@gmail.com>
2024-06-26 11:40:40 -0300

    ajuste cors do lado rest-api, retirada de configurações daqui

--- script.js
+++ script.js
@@ -2,26 +2,12 @@ const clickButton = document.getElementById('clickButton');
 const clickCount = document.getElementById('clickCount');
 const apiResponse = document.getElementById('apiResponse');
 
-const options = {
-  method: 'GET',
-  url: 'http://3.238.219.121:3000/msg',
-  Headers: {
-    'Content-Type': 'application/json',
-  },
-  cache: 'no-cache',
-};
-
 let count = 0;
 
 clickButton.addEventListener('click', () => {
   count++;
   clickCount.textContent = `Cliques: ${count}`;
-  
-  
-  // teste
-  // URL do proxy CORS Anywhere
-  // const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-  const targetUrl = 'http://3.238.219.121:3000/msg';
+  const targetUrl = 'http://50.19.158.248:3000/msg';
   
   // Chamada da API via proxy
   // fetch(targetUrl)
